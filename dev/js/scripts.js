cda7eb2cfd462ca9cd3ab296e14dd3ee9bf745f5
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-10-06 23:12:01 -0400

    push

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,7 @@
 import {headerIntroAnimation} from "./section-1.js";
 import {sectionTwoAnimation, twoParaAnimation, twoImgAnimation} from "./section-2.js";
 import {sectionThreeAnimation, threeParaAnimation, threeImgAnimation} from "./section-3.js";
+import {showCaseAnimation, fourImgAnimation, fourParaAnimation} from "./section-4.js";
 
 
 
@@ -12,6 +13,9 @@ sectionThreeAnimation();
 threeParaAnimation();
 threeImgAnimation();
 twoImgAnimation();
+showCaseAnimation();
+fourImgAnimation();
+fourParaAnimation();
 
 
 
---
+++ dev/js/section-4.js
@@ -0,0 +1,53 @@
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const showcaseTimeTL = gsap.timeline();
+
+showcaseTimeTL.from("#header4",{duration: 6, x: 500});
+
+export function showCaseAnimation(){
+    ScrollTrigger.create({
+        trigger: "#header4",
+        animation: showcaseTimeTL,
+        start: "top 70%",
+        end: "top 70%",
+        scrub: 3,
+        markers: true
+    });
+
+    
+}
+
+const fourthImgTL = gsap.timeline();
+
+fourthImgTL.from("#indie",{duration:3, x:-1800});
+
+export function fourImgAnimation(){
+    ScrollTrigger.create({
+        markers: true,
+        animation: fourthImgTL,
+        trigger: "#indie",
+        start:"top, 100%",
+        end: "bottom, 40%",
+        scrub: 2
+    });
+}
+
+const fourPTL = gsap.timeline();
+
+fourPTL.from("#p4",{duration: 6, x: 500});
+
+export function fourParaAnimation(){
+    ScrollTrigger.create({
+        trigger: "#p4",
+        animation: fourPTL,
+        start: "top 70%",
+        end: "top 70%",
+        scrub: 3,
+        markers: true
+    });
+
+    
+}
